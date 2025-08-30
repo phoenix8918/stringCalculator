ca6eaf907dbbd1d28658503bb34834d1ec58604b
Prasanth M <phoenixsakthiprasanth@gmail.com>
2025-08-30 23:01:00 +0530

    handle new lines between numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i unless numbers.include?(',')
 
-    nums = numbers.split(",").map(&:to_i)
+    nums = numbers.split(/,|\n/).map(&:to_i)
     nums.sum
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "returns sum of multiple numbers" do
       expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
+
+    it "handle new lines between numbers" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
